6cacba717385f217ec68f8953ba2add0784c30ca
QaaliAden <46288896+QaaliAden@users.noreply.github.com>
2020-05-18 14:47:39 +0100

    Add files via upload

---
+++ eidproject.js
@@ -0,0 +1,12 @@
+ function EidMubarak(){
+  var txtName = document.getElementById("txtName");
+  var txtOutput = document.getElementById("txtOutput");
+  var name = txtName.value;
+  txtOutput.value =  name 
+  } // end sayHi
+  $('section').fireworks({ 
+  sound: true, // sound effect
+  opacity: 0.9, 
+  width: '100%', 
+  height: '100%' 
+});
